72b343b62243c2fd329a9b34675a454461852e42
Stenka <62915120+stenkarazin2@users.noreply.github.com>
2025-04-25 04:35:30 +0300

    Added test SELECT 1

--- script.py
+++ script.py
@@ -16,14 +16,21 @@ print( hosts[0] + "\'s \%idle: " + host_idle[hosts[0]] + "   " +
 if host_idle[hosts[0]] > host_idle[hosts[1]]:
   target_host = hosts[0]
 else:
-  target_host = hosts[1]
+  target_host = hosts[0]
 print( "...... host " + target_host + " chosen!" )
 #-------------------------------------------------------
 print( "Installing PostgreSQL ..." )
-command = "ansible-playbook -u root -i " + target_host + ", playbooks/install_postgresql.yml" # > /dev/null"
+command = "ansible-playbook -u root -i " + target_host + ", playbooks/install_postgresql.yml --extra-vars \"second_host_ip_address=" + hosts[1] + "\" > /dev/null 2> /dev/null"
 if os.system( command ) == 0:
   print( "...... PostgreSQL installed!" )
 else:
   print( "  Error! PostgreSQL not installed :(" )
   exit()
 #-------------------------------------------------------
+print( "Checking PostgreSQL (sending SELECT 1)" )
+command = "ansible-playbook -u root -i " + target_host + ", playbooks/check_postgresql.yml > /dev/null 2> /dev/null"
+if os.system( command ) == 0:
+  print( "...... PostgreSQL checked!" )
+else:
+  print( "  Error! PostgreSQL check failed :(" )
+  exit()
