22060eb0061bf020c3d1b9371304806dc2de697d
Stenka <62915120+stenkarazin2@users.noreply.github.com>
2025-05-02 02:17:28 +0300

    Added hostnames processing

--- script.py
+++ script.py
@@ -5,22 +5,27 @@ hosts = argv[1].split(',')
 #-------------------------------------------------------
 print( "Choose host for installing ..." )
 
-filter = '\'\"' + argv[1].replace(',', "\"|\"") + '\"\''
-command = "ansible-playbook -u root -i " + argv[1] + " playbooks/choose_host.yml | grep -P " + filter 
+filter = '\'\"' + argv[1].replace(',', "\"|\"") + '\"|\"' + hosts[1] + hosts[1] + '\":\''
+command = "ansible-playbook -u root -i " + argv[1] + " playbooks/choose_host.yml --extra-vars \"second_host=" + hosts[1] + "\" | grep -P " + filter
 str1 = str( subprocess.check_output( command, shell=True), errors='ignore' )
-str2 = '{' + str1.replace('\n', ',', 1) + '}'
-host_idle = json.loads(str2)
+str2 = '{' + str1.replace('\n', ',', 2) + '}'
+host_info = json.loads(str2)
 
-print( hosts[0] + "\'s \%idle: " + host_idle[hosts[0]] + "   " +
-       hosts[1] + "\'s \%idle: " + host_idle[hosts[1]] )
-if host_idle[hosts[0]] > host_idle[hosts[1]]:
+print( hosts[0] + "\'s \%idle: " + host_info[hosts[0]] + "   " +
+       hosts[1] + "\'s \%idle: " + host_info[hosts[1]] )
+if host_info[hosts[0]] > host_info[hosts[1]]:
   target_host = hosts[0]
 else:
   target_host = hosts[1]
 print( "...... host " + target_host + " chosen!" )
 #-------------------------------------------------------
+second_host_ip_address = host_info[hosts[1] + hosts[1]]
+if second_host_ip_address != hosts[1]:
+  print( "Looking up IP address of the second host for student's access" )
+  print( "...... second host's IP address is " + second_host_ip_address + "!" )
+#-------------------------------------------------------
 print( "Installing PostgreSQL ..." )
-command = "ansible-playbook -u root -i " + target_host + ", playbooks/install_postgresql.yml --extra-vars \"second_host_ip_address=" + hosts[1] + "\" > /dev/null 2> /dev/null"
+command = "ansible-playbook -u root -i " + target_host + ", playbooks/install_postgresql.yml --extra-vars \"second_host_ip_address=" + second_host_ip_address + "\" > /dev/null 2> /dev/null"
 if os.system( command ) == 0:
   print( "...... PostgreSQL installed!" )
 else:
